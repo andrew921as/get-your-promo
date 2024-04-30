e7ec26b6c2ed763469b6842cba5e1e5ef8b84a60
Jose Gabriel Jimenez Vidales <Jose-Gabriel.Jimenez@endava.com>
2024-04-29 23:30:15 -0500

    feat: create alkosto endpoint to retrieve promos

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
 import corse from 'cors';
+import { chromium } from 'playwright';
 
 const app = express();
 app.disable('x-powered-by');
@@ -9,7 +10,29 @@ app.use(corse());
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.send('Welcome the Get Your Promo API!');
+});
+
+app.get('/alkosto', async (req, res) => {
+    const browser = await chromium.launch();  // Launch the browser
+    const page = await browser.newPage();     // Open a new page
+
+    await page.goto('https://www.alkosto.com/mercado-tiendas-fisicas');  // Navigate to the URL
+
+    // Find an element by its accessibility name
+	const element = await page.locator('role=link[name="mercado Descarga Catalogo aquí"]');
+
+
+    if (await element.count() > 0) {
+        const href = await element.getAttribute('href');  // Fetch the href attribute of the element
+        console.log(href);  // Print the href
+        res.send({ response: href});
+    } else {
+        console.log("No element found with the specified accessibility name.");
+        res.send({ response: "No element found with the specified accessibility name."}).status(404);
+    }
+
+    await browser.close();  // Close the browser
 });
 
 app.listen(PORT, () => {
